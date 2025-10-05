fc3e8990ccf1ae8460d3bcb86178ce8172159288
Alfredo Oltra <aoltra@gmail.com>
2025-10-05 10:11:17 +0000

    feat: scaffolding

---
+++ __manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Maya | Students",
+    'version': '17.0.1.0',
+
+    'summary': """
+         Extensión de Maya | Core para la gestión de estudiantes""",
+
+    'description': """
+        Permite:
+         - el control de las anulaciones de matrícula de oficio por inactividad
+    """,
+
+    'author': "Alfredo Oltra",
+    'website': "https://portal.edu.gva.es/ceedcv/",
+    'maintainer': 'Alfredo Oltra <alfredo.ptcf@gmail.com>',
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Productivity',
+
+    'license': 'AGPL-3',
+    'price': 0,
+
+    # any module necessary for this one to work correctly
+     # any module necessary for this one to work correctly
+    'depends': ['base', 'mail', 'maya_core'],
+
+    # always loaded
+    'data': [
+        #'security/ir.model.access.csv',
+        #'views/views.xml',
+        #'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
