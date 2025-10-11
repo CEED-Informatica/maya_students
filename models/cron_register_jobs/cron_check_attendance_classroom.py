4e528b94d95daccd2fc8e742443c5801d0250f5d
Alfredo Oltra <aoltra@gmail.com>
2025-10-11 20:56:09 +0000

    feat: se filtran los alumnos que no se hayan conectado en N dias

--- models/cron_register_jobs/cron_check_attendance_classroom.py
+++ models/cron_register_jobs/cron_check_attendance_classroom.py
@@ -10,6 +10,7 @@ from ....maya_core.support.maya_logger.exceptions import MayaException
 from ....maya_core.support.maya_moodleteacher.maya_moodle_connection import MayaMoodleConnection
 from ....maya_core.support.maya_moodleteacher.maya_moodle_user import MayaMoodleUsers
 
+from ....maya_core.models.cron_register_jobs.cron_job_enrol_users import CronJobEnrolUsers
 
 _logger = logging.getLogger(__name__)
 
@@ -17,7 +18,7 @@ class CronCheckAttendanceClassroom(models.TransientModel):
   _name = 'maya_students.cron_check_attendance_classroom'
 
   @api.model
-  def cron_check_attendance_classroom(self, check_classrooms_id: list[tuple[int,str]], course_id: int):
+  def cron_check_attendance_classroom(self, check_classrooms_id: list[tuple[int,int]], course_id: int):
 
     # ŧODO ponerlo en configuraciones
     set_midnight = True
@@ -28,7 +29,7 @@ class CronCheckAttendanceClassroom(models.TransientModel):
       _logger.error("CRON: check_classrooms_id no definido")
       return
     
-    _logger.info(f"Comprobando asistencia en el ciclo {course_id}. Número de aulas: {len(check_classrooms_id)}")
+    print(f'\033[0;34m[INFO]\033[0m Comprobando asistencia en el ciclo {course_id}. Número de aulas: {len(check_classrooms_id)}')
 
     current_sy = (self.env['maya_core.school_year'].search([('state', '=', 1)])) # curso escolar actual  
 
@@ -37,7 +38,7 @@ class CronCheckAttendanceClassroom(models.TransientModel):
         user = self.env['ir.config_parameter'].get_param('maya_core.moodle_user'), 
         moodle_host = self.env['ir.config_parameter'].get_param('maya_core.moodle_url')) 
     except Exception as e:
-      raise Exception('No es posible realizar la conexión con Moodle' + e)
+      raise Exception('No es posible realizar la conexión con Moodle' + str(e))
     
 
     if len(current_sy) == 0:
@@ -52,17 +53,38 @@ class CronCheckAttendanceClassroom(models.TransientModel):
     current_datetime = datetime.now()
     current_day = current_datetime.strftime('%d-%m-%Y %H:%M:%S')
 
-    _logger.info(f'Fecha actual:', current_day) 
+    print(f'\033[0;34m[INFO]\033[0m Fecha actual: {current_day}')
+
+    if set_midnight:
+      # Calculo la fecha límite: medianoche de N días antes
+      deadline = current_datetime.replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=days)
+    else:
+      deadline = current_datetime - timedelta(days=days)
 
     for classroom in check_classrooms_id:
       print('\033[0;34m[INFO]\033[0m Obteniendo usuarios del aula ->', classroom[1])  
       
       # obtención de los usuarios 
       users = MayaMoodleUsers.from_course(conn,  classroom[0], only_students = True)
+      risk_users = []
 
-      risk_users = 0
-      if set_midnight:
-        # Calculo la fecha límite: medianoche de N días antes
-        deadline = current_datetime.replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=days)
-      else:
-        deadline = current_datetime - timedelta(days=days)
+      for user in users:
+        lastcourseaccess = getattr(user, "lastcourseaccess", None)
+
+        if not lastcourseaccess or lastcourseaccess == 0:
+          access_datetime =  datetime.min
+        else:
+          access_datetime = datetime.fromtimestamp(int(lastcourseaccess))
+        
+        if access_datetime < deadline:
+          risk_users.append(user)
+
+    
+      for user in risk_users:
+        # los matriculamos en Maya si no lo están
+        a_user =  CronJobEnrolUsers.enrol_student(self, user, classroom[1], course_id) 
+
+        
+
+        
+              
